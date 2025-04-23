8380854d7e0fc7824fc507456dd068ef3cdb1795
Wojtek Galik <120197999+wgalik@users.noreply.github.com>
2025-04-23 12:39:24 +0200

    Add files via upload

--- wizytowka/js/main.js
+++ wizytowka/js/main.js
@@ -4,10 +4,11 @@ const langBtnPL = document.querySelector("#langBtnPL");
 const langBtnEN = document.querySelector("#langBtnEN");
 const main_pl = document.querySelector("#main_pl");
 const main_en = document.querySelector("#main_en");
-console.log(10);
+const logoWP = document.querySelectorAll('.logoWP');
+const logoGH = document.querySelector('#logoGH');
 
 let isPL = true;
-
+let isDark = false;
 langBtnPL.addEventListener("click", () => {
   console.log(101);
 
@@ -28,5 +29,30 @@ langBtnEN.addEventListener("click", () => {
     langBtnEN.style.display = "none";
     langBtnPL.style.display = "block";
     isPL = true;
+  }
+});
+
+day.addEventListener("click", function () {
+  if (isDark) {
+    day.style.display = "none";
+    night.style.display = "block";
+    document.body.classList.remove("dark-mode");
+    logoWP.forEach((el) => {
+      el.style.filter = "brightness()";
+    });
+    logoGH.style.filter = "brightness()";
+    isDark = false;
+  }
+});
+night.addEventListener("click", function () {
+  if (!isDark) {
+    night.style.display = "none";
+    day.style.display = "block";
+    document.body.classList.add("dark-mode");
+    logoWP.forEach((el) => {
+      el.style.filter = "brightness(5)";
+    });
+    logoGH.style.filter = "brightness(10)";
+    isDark = true;
   }
 });
