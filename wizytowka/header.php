073cffe5ec7fe0e5eb059542ce29e421094b26b5
Wojtek Galik <120197999+wgalik@users.noreply.github.com>
2025-04-25 18:34:01 +0200

    Update header.php

--- wizytowka/header.php
+++ wizytowka/header.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="pl">
 <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php wp_head(); ?>
   <title><?php bloginfo('name'); ?></title>
   <style>
